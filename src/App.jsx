23450fc865499926ab51d31ffaecbd22953fce9e
k-kinzal <log@logn.in>
2024-03-09 17:13:25 +0900

    initialized theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import "./App.css";
+import { ThemeProvider } from "./providers/Theme.jsx";
+import { CssBaseline } from "@mui/material";
 
 export function App() {
-  return <div>aaa</div>;
+  return (
+    <ThemeProvider>
+      <CssBaseline />
+      <div>aaa</div>
+    </ThemeProvider>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
-import { CssBaseline } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CssBaseline>
-      <App />
-    </CssBaseline>
+    <App />
   </React.StrictMode>,
 );
